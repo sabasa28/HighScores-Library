4f5dc206ed799cee6d3598541bcbc1b5a3a35857
Iñaki Diez Galarza <inakidiezgalarza@gmail.com>
2020-04-21 13:07:44 -0300

    Creado proyecto en visual studio

---
+++ HScores/HScores/HScores.cpp
@@ -0,0 +1,17 @@
+#include "HScores.h"
+
+#include "pch.h"
+#include <iostream>
+#include <list>
+
+
+using namespace std;
+
+namespace scrs
+{
+	static list<float> auxList;
+	static void updateList()
+	{
+		
+	}
+}
---
+++ HScores/HScores/HScores.h
@@ -0,0 +1,18 @@
+#ifndef HSCORES_H
+#define HSCORES_H
+
+namespace scrs
+{
+#pragma region vars
+	list<float> scores;
+
+#pragma endregion Variables
+
+#pragma region funcs
+
+
+#pragma endregion Funciones
+}
+
+#endif //HSCORES_H
+
